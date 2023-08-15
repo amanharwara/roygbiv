225e642ed7dfe08cb5dcf8640ebfd91d29e9ea40
Aman Harwara <amanharwara@protonmail.com>
2023-08-16 02:31:13 +0530

    add solid-js and tailwind

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "solid-js/web";
+import App from "./App";
+
+const root = document.getElementById("root");
+
+render(() => <App />, root!);
--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [solidPlugin()],
+  build: {
+    target: "esnext",
+  },
+});
