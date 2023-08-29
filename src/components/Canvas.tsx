5d86c008188213b102fe980d4bc4d63aebfbd16f
Aman Harwara <amanharwara@protonmail.com>
2023-08-29 14:59:09 +0530

    number field stuff

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -76,7 +76,6 @@ export function CanvasSettingsModal() {
 
   return (
     <Popover
-      isOpen
       placement="bottom end"
       className="rounded border border-neutral-700 bg-neutral-800 p-3 data-[entering]:animate-fade-in data-[exiting]:animate-fade-out "
     >
--- src/components/NumberField.tsx
+++ src/components/NumberField.tsx
@@ -25,14 +25,14 @@ const NumberField = ({ label, className, ...props }: Props) => {
           <Label>{label}</Label>
           <Group className="flex w-fit rounded">
             <Button
-              className="rounded rounded-ee-none rounded-se-none border border-neutral-500 bg-neutral-700 px-2.5 hover:bg-neutral-600"
+              className="rounded rounded-ee-none rounded-se-none border border-neutral-600 bg-neutral-700 px-2.5 hover:bg-neutral-600"
               slot="decrement"
             >
               -
             </Button>
-            <Input className="border border-neutral-500 bg-neutral-700 px-2 py-1.5 text-sm outline-none group-focus-within:border-neutral-300" />
+            <Input className="z-[1] -mx-px border border-neutral-600 bg-neutral-700 px-2 py-1.5 text-sm outline-none group-focus-within:border-neutral-300" />
             <Button
-              className="rounded rounded-es-none rounded-ss-none border border-neutral-500 bg-neutral-700 px-2.5 hover:bg-neutral-600"
+              className="rounded rounded-es-none rounded-ss-none border border-neutral-600 bg-neutral-700 px-2.5 hover:bg-neutral-600"
               slot="increment"
             >
               +
@@ -43,7 +43,7 @@ const NumberField = ({ label, className, ...props }: Props) => {
                   state.setNumberValue(props.defaultValue);
                 }
               }}
-              className="ml-1.5 rounded border border-neutral-500 bg-neutral-700 px-2 hover:bg-neutral-600"
+              className="ml-1.5 rounded border border-neutral-600 bg-neutral-700 px-2 hover:bg-neutral-600"
             >
               <ResetIcon className="h-3.5 w-3.5" />
             </button>
