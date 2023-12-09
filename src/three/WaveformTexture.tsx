d3a8759dc38f7dea1ab7933ededa5c095074a84c
Aman Harwara <amanharwara@protonmail.com>
2023-12-09 09:02:12 +0530

    useFrame instead of useEffect

--- src/three/WaveformTexture.tsx
+++ src/three/WaveformTexture.tsx
@@ -1,5 +1,5 @@
-import { useThree } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
 import { getTimeDomainData, useAudioStore } from "../stores/audio";
 import { CanvasTexture } from "three";
 
@@ -14,56 +14,42 @@ export function WaveformTexture() {
 
   const isPlaying = useAudioStore((state) => state.isPlaying);
 
-  useEffect(() => {
-    let animationFrameId: number;
+  useFrame(() => {
+    if (!isPlaying) return;
 
-    const updateCanvas = () => {
-      if (!isPlaying) return;
+    const canvas = canvasElement.current;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
 
-      const canvas = canvasElement.current;
-      const ctx = canvas.getContext("2d");
-      if (!ctx) return;
+    const width = canvas.width;
+    const height = canvas.height;
 
-      const width = canvas.width;
-      const height = canvas.height;
+    const timeDomainData = getTimeDomainData();
 
-      const timeDomainData = getTimeDomainData();
+    ctx.clearRect(0, 0, width, height);
 
-      ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
 
-      ctx.beginPath();
+    ctx.moveTo(0, height / 2);
 
-      ctx.moveTo(0, height / 2);
+    const sliceWidth = (width * 1.0) / timeDomainData.length;
 
-      const sliceWidth = (width * 1.0) / timeDomainData.length;
+    let x = 0;
 
-      let x = 0;
-
-      for (const sample of timeDomainData) {
-        const y = (sample / 255.0) * height;
-        ctx.lineTo(x, y);
-        x += sliceWidth;
-      }
-
-      ctx.lineTo(width, height / 2);
-
-      ctx.strokeStyle = "#ffffff";
-
-      ctx.stroke();
-
-      ref.current!.needsUpdate = true;
-
-      animationFrameId = requestAnimationFrame(updateCanvas);
-    };
-
-    if (isPlaying) {
-      animationFrameId = requestAnimationFrame(updateCanvas);
+    for (const sample of timeDomainData) {
+      const y = (sample / 255.0) * height;
+      ctx.lineTo(x, y);
+      x += sliceWidth;
     }
 
-    return () => {
-      cancelAnimationFrame(animationFrameId);
-    };
-  }, [isPlaying]);
+    ctx.lineTo(width, height / 2);
+
+    ctx.strokeStyle = "#ffffff";
+
+    ctx.stroke();
+
+    ref.current!.needsUpdate = true;
+  });
 
   return (
     <canvasTexture
